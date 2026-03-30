a922125f6cd88ed6fd2f5b0186d2574c6292f182
Wasee <229995823+waseeconsulting-git@users.noreply.github.com>
2026-03-31 00:55:01 +0200

    evidence

--- test-bucket.tf
+++ test-bucket.tf
@@ -18,6 +18,28 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+locals {
+  g-check_evidence = {
+    "armageddon.txt"                     = "text/plain"
+    "Destroy-complete.JPG"              = "image/jpg"
+    "Apply-complete.JPG"            = "image/jpg"
+    "SUCCESS.JPG"             = "image/jpg"
+    "webhook-delivery.JPG"          = "image/jpg"
+    "u6KxR-1W.jpg" = "image/jpg"
+    "wub8Qfph.jpg" = "image/jpg"
+  }
+}
+
+resource "aws_s3_object" "submission_evidence" {
+  for_each = local.g-check_evidence
+
+  bucket       = aws_s3_bucket.frontend.id
+  key          = each.key
+  source       = "${path.module}/evidence/${each.key}"
+  content_type = each.value
+  source_hash  = filemd5("${path.module}/evidence/${each.key}")
+}
+
 resource "aws_s3_bucket" "frontend" {
   bucket_prefix = "jenkins-bucket-"
   force_destroy = true
