99e325e3b9032e1b391e91dd1e0151fc75b45b82
Wasee <229995823+waseeconsulting-git@users.noreply.github.com>
2026-04-02 20:47:46 +0200

    test-bucket.tf update public access

--- test-bucket.tf
+++ test-bucket.tf
@@ -50,3 +50,30 @@ resource "aws_s3_bucket" "frontend" {
     Name = "Jenkins Bucket"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "assets_access" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "assets_read_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.assets_access]
+  bucket     = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::YOUR_BUCKET/*"
+      }
+    ]
+  })
+}
